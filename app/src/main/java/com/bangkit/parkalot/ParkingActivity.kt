991b73b0c738c9003ef48782e04f212bc3a6cd73
KhaironDaffa <dkhairon@gmail.com>
2021-06-08 20:00:39 +0700

    Update Details

--- app/src/main/java/com/bangkit/parkalot/ParkingActivity.kt
+++ app/src/main/java/com/bangkit/parkalot/ParkingActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import com.bangkit.parkalot.databinding.ActivityParkingBinding
 
 class ParkingActivity : AppCompatActivity(), View.OnClickListener {
@@ -19,6 +20,22 @@ class ParkingActivity : AppCompatActivity(), View.OnClickListener {
         supportActionBar?.hide()
 
         parkingBinding.btnBack.setOnClickListener(this)
+
+        val spotA1 = parkingBinding.A1
+        val spotA2 = parkingBinding.A2
+        val spotA3 = parkingBinding.A3
+        val spotA4 = parkingBinding.A4
+        val spotA5 = parkingBinding.A5
+        val spotA6 = parkingBinding.A6
+        val indicatorTrue = 1
+        val indicatorFalse = 0
+
+        indicator(indicatorFalse, spotA1)
+        indicator(indicatorTrue, spotA2)
+        indicator(indicatorFalse, spotA3)
+        indicator(indicatorFalse, spotA4)
+        indicator(indicatorFalse, spotA5)
+        indicator(indicatorTrue, spotA6)
     }
 
     override fun onClick(v: View?) {
@@ -30,9 +47,13 @@ class ParkingActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun indicator(indicator: Int) {
+    private fun indicator(indicator: Int, spot: TextView) {
         if (indicator == 0){
+            spot.setBackgroundResource(R.drawable.spot_layout_green)
+        }
 
+        else {
+            spot.setBackgroundResource(R.drawable.spot_layout_red)
         }
     }
 }
